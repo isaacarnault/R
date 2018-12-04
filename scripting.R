f946fd65db1a71052ad6fa15724a636e3c21d367
Isaac Arnault <44641020+aiPhD@users.noreply.github.com>
2018-12-04 19:57:50 +0100

    Update scripting.R

--- scripting.R
+++ scripting.R
@@ -1,24 +1,25 @@
 # Reading our dataset
-MyData <- read.csv(file="/home/zaki/Desktop/countries_R.csv",
+MyData <- read.csv(file="dataset_hadoop.csv",
           header=TRUE, sep=",")
+MwData
 
-# Performing first visualization using `.ggvis` library. We use `layer_points` function.
+# Performing 1st visualization using `.ggvis` library. Function: layer_points.
 MyData %>% 
   ggvis(~Server, ~Nodes) %>%
   layer_points()
 
-# Improving the above script by sorting the graph per Server per Nodes per Company. We call `layer_points` function.
+# Performing 2nd visualization by highlighting Server per Nodes per Company. The script improves the output of the above script.
 MyData %>% 
   ggvis(~Server, ~Nodes) %>%
   layer_points() %>%
   layer_points(fill = ~Company)
 
-# Performing third visualization using `.ggvis` library. We call `layer_points` function
+# Performing 3rd visualization using `.ggvis` library. Function: layer_points.
 MyData %>% 
   ggvis(~IP, ~Nodes) %>% 
   layer_points(size := 25, shape := "diamond", stroke := "red", fill := NA)
 
-# Performing fourth visualization using `.ggvis` library. We call `layer_points` function
+# Performing 4th visualization using `.ggvis` library. Functions: layer_lines, layer_points.
 MyData %>%
   ggvis(~IP, ~Nodes, stroke := "skyblue",
         strokeOpacity := 0.5, strokeWidth := 5) %>%
@@ -26,3 +27,11 @@ MyData %>%
   layer_points(fill = ~Company,
                shape := "triangle-up",
                size := 300)
+
+# Performing 5th visualization using `.ggplot` library. Function: geom_bar.
+g <- ggplot(MyData, aes(Company))
+g + geom_bar(aes(fill=Nodes), width = 0.5) + 
+  theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+  labs(title="Using ggplot - Histogram on a categorical variable", 
+       subtitle="IP Vs Nodes", 
+       caption="Author: aiPhD")

3d9fd1a0e563b61a3386c37f9d812bf7103b4053
Isaac Arnault <44641020+aiPhD@users.noreply.github.com>
2018-12-04 20:13:11 +0100

    Update scripting.R

--- scripting.R
+++ scripting.R
@@ -8,18 +8,18 @@ MyData %>%
   ggvis(~Server, ~Nodes) %>%
   layer_points()
 
-# Performing 2nd visualization by highlighting Server per Nodes per Company. The script improves the output of the above script.
+# ggvis: 2nd visualization using layer_points
 MyData %>% 
   ggvis(~Server, ~Nodes) %>%
   layer_points() %>%
   layer_points(fill = ~Company)
 
-# Performing 3rd visualization using `.ggvis` library. Function: layer_points.
+# ggvis: 3rd visualization using layer_points
 MyData %>% 
   ggvis(~IP, ~Nodes) %>% 
   layer_points(size := 25, shape := "diamond", stroke := "red", fill := NA)
 
-# Performing 4th visualization using `.ggvis` library. Functions: layer_lines, layer_points.
+# ggvis: 4th visualization using layer_lines, layer_points
 MyData %>%
   ggvis(~IP, ~Nodes, stroke := "skyblue",
         strokeOpacity := 0.5, strokeWidth := 5) %>%
@@ -28,7 +28,7 @@ MyData %>%
                shape := "triangle-up",
                size := 300)
 
-# Performing 5th visualization using `.ggplot` library. Function: geom_bar.
+# ggplot: 1st visualization using geom_bar
 g <- ggplot(MyData, aes(Company))
 g + geom_bar(aes(fill=Nodes), width = 0.5) + 
   theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
